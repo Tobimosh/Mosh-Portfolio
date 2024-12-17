83cd01812e153261a0a919d5130b0b679b04f790
Moshood Oluwatobi <oluwatobimoshood16@gmail.com>
2024-12-17 17:11:22 +0100

    about section (ongoing)

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Spotlight } from "../ui/Spotlight";
 import { TextGenerateEffect } from "../ui/TextGenerateEffect";
+import MagicButton from "../ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -16,20 +18,28 @@ const Hero = () => {
         />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw] " fill="blue" />
       </div>
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center">
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.06] bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
-      <div
-      className="flex justify-center my-20 relative z-10"
-      >
+      <div className="flex justify-center my-20 relative z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw]  flex flex-col justify-center items-center">
-            <h2 className="tracking-widest text-xs text-center text-blue-100 max-w-80 uppercase">Dynamic web magic with next.js </h2>
-            <TextGenerateEffect className="text-center text-[40px] md:text-5xl capitalize  lg:text-6xl" words="Shaping concepts into seamless solutions"/>
-            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl  ">
-                Hi, I&apos;m Mosh, a software engineer based in Nigeria
-                
-            </p>
-
+          <h2 className="tracking-widest text-xs text-center text-blue-100 max-w-80 uppercase">
+            Dynamic web magic with next.js{" "}
+          </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl capitalize  lg:text-6xl"
+            words="Shaping concepts into seamless solutions"
+          />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl  ">
+            Hi, I&apos;m Mosh, a software engineer based in Nigeria
+          </p>
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
         </div>
       </div>
     </div>
